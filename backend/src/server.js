06ed2b2600ec3df3a41aed023431ece189076d7b
Faizan Ali Khan <alikhanfaizan642@gmail.com>
2025-11-16 16:53:49 +0530

    Deploy config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
+import path from 'path';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World! from merge conflict');
+const __dirname = path.resolve();
+
+app.get('/health', (req, res) => {
+  res.status(200).send("API is working");
+});
+app.get('/books', (req, res) => {
+  res.status(200).send("List of books");
 });
 
+
+if(ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
 });
