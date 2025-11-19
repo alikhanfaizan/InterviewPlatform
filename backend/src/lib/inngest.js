36b649a66e353d00eeeff62faafbb8d1e7b5ce40
Faizan Ali Khan <alikhanfaizan642@gmail.com>
2025-11-19 15:39:43 +0530

    minor changes

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -19,7 +19,7 @@ const syncUserData = inngest.createFunction(
     };
     await User.create(newUser);
     await upsertStreamUser({
-      id: newUser.clerkId.to_string(),
+      id: newUser.clerkId.toString(),
       name: newUser.name,
       image: newUser.profileImage,
     });
