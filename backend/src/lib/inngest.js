ec2c9b3a409de6a5ade8d112f7689884a6bc34f2
Faizan Ali Khan <alikhanfaizan642@gmail.com>
2025-11-19 14:52:03 +0530

    Inngest Integrations

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,35 @@
+import {Inngest} from "inngest";
+import User from "../models/User.js";
+import { connectDB } from "./db.js";
+
+export const inngest = new Inngest({ id: "my-INterviewPrep" });
+const syncUserData = inngest.createFunction(
+   {id: "sync-user"},
+   { event: "clerk/user.created" },
+    async ({ event }) => {
+        await connectDB();
+        const {id,email_addresses,first_name,last_name,image_url}=event.data;
+        const newUser = {
+            clerkId: id,
+            email: email_addresses[0]?.email_address ,
+            name: `${first_name || ""} ${last_name || ""}`,
+            profileImage: image_url,
+        }
+        await User.create(newUser);
+        console.log("User synced to DB:", newUser);
+    }
+
+)
+const deleteUserFromDb = inngest.createFunction(
+   {id: "delete-user-from-db"},
+   { event: "clerk/user.deleted" },
+    async ({ event, step }) => {
+        await connectDB();
+        const {id}=event.data;
+        
+        await User.deleteOne({clerkId:id});
+        // console.log("User synced to DB:", newUser);
+    }
+
+)
+export const functions = [ syncUserData , deleteUserFromDb];
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,24 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import {serve} from "inngest/express";
+import { inngest,functions } from "./lib/inngest.js";
+
+
+
 const app = express();
 
 const __dirname = path.resolve();
+
+//middlewares
+app.use(express.json());
+app.use(cors(
+  {
+    origin: ENV.CLIENT_URL ,
+    credentials: true,
+  }
+));
 
 app.get("/health", (req, res) => {
   res.status(200).send("API is working");
@@ -12,6 +27,11 @@ app.get("/health", (req, res) => {
 app.get("/books", (req, res) => {
   res.status(200).send("List of books");
 });
+
+app.use("/api/inngest", serve({
+  client:inngest,functions
+}));
+
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
